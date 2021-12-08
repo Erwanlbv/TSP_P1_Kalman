10239129877a64012a4af3e5bde3a7051662a685
Erwanlbv <er.leblevec@gmail.com>
2021-12-08 21:25:20 +0100

    Réecriture fonctions en vectoriel

--- Partie_I/main_p1.py
+++ Partie_I/main_p1.py
@@ -2,19 +2,15 @@ import numpy as np
 
 
 def creer_trajectoire(F, Q, x_init, T): #Format 4*n
-    trajectoire = [np.transpose(x_init)]
+    trajectoire = np.zeros((100, 4), dtype='float64')
+    trajectoire[0] = x_init
     for i in range(1, T):
-        trajectoire.append(
-            F.dot(trajectoire[-1]) +
-            np.random.multivariate_normal(np.zeros((4, 1), dtype='float64').flatten(), Q)
-        )
-    return np.transpose(trajectoire)
+        trajectoire[i] = np.transpose(F.dot(trajectoire[i-1]))
+    return np.transpose(trajectoire) + np.transpose(np.random.multivariate_normal(np.zeros(4), Q, 100))
 
 
 def creer_observation(H, R, vecteur_x): #Format 2*n
-    return(
-        H.dot(vecteur_x) + np.transpose(np.random.multivariate_normal(np.zeros((2, 1), dtype='float64').flatten(), R, len(vecteur_x[0])))
-    )
+    return H.dot(vecteur_x) + np.transpose(np.random.multivariate_normal(np.zeros(2), R, len(vecteur_x[0])))
 
 
 def filtre_de_kalman(F, Q, H, R, y_k, x_kalm_prec, P_kalm_prec):
@@ -30,20 +26,6 @@ def filtre_de_kalman(F, Q, H, R, y_k, x_kalm_prec, P_kalm_prec):
     x_filt_suiv = x_pred_suiv + K_filt_suiv.dot(y_k - H.dot(x_pred_suiv))
 
     return [x_filt_suiv, P_filt_suiv]
-
-
-#Fonction intermédiaire pour faciliter les calculs :
-
-
-def get_P_and_K(P_init, H, Q, R, F, n):
-    results = [[P_init], [P_init.dot(np.transpose(H).dot(np.linalg.inv(H.dot(P_init.dot(np.transpose(H))) + R)))]]
-
-    for i in range(1, n):
-        results[0].append(F.dot(results[0][-1].dot(np.transpose(F))) + Q)
-        results[1].append(
-            results[0][-1].dot(np.transpose(H).dot(np.linalg.inv(H.dot(results[0][-1].dot(np.transpose(H))) + R)))
-        )
-    return results
 
 
 def err_quadra(x_real, x_est): #Format 4*1
